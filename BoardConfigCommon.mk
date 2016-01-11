eb8d9afaa8bded64a9859b648dcb3073c50dfbd0
doc HD <doc.divxm@gmail.com>
2016-01-11 16:52:20 +0200

    serrano: Add TWRP flags
    With special thanx to @F4k :)
    
    Change-Id: If78174ff1638939fb959b9493b2589d398e8a573

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,6 +42,36 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/serrano-common/rootdir/fstab.qcom
 
+# TWRP
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
+
+TW_THEME := portrait_hdpi
+DEVICE_RESOLUTION := 540x960
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_LCD_BLANK_PATH := \"/sys/class/lcd/panel/backlight\"
+TW_BRIGHTNESS_PATH := "/sys/devices/platform/mipi_samsung_oled.1281/lcd/panel/backlight"
+TW_MAX_BRIGHTNESS := 255
+
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_USB_STORAGE := true
+
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_SAMSUNG := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,discard,journal_async_commit,errors=panic"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+TW_INCLUDE_FUSE_EXFAT := true
+
 # Reduce space taken by the journal
 BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
 
